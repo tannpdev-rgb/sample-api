8419ce6e35d9762ce067781e96f7a9511ceba1f1
tandev <tannp.dev@gmail.com>
2026-01-11 12:10:43 +0700

    add compose

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/chroma", async (req, res) => {
+  try {
+    const response = await axios.get("http://chroma:8000/api/v1/heartbeat")
+    res.json(response.data)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
